9ab45e68d3013ece210b58aeebd534ce36dbdb1c
Sarthak Sidhant <52284479+Sarthak-Sidhant@users.noreply.github.com>
2024-05-08 23:15:33 +0530

    Update 30100pulsetest.py

--- cow - physical module/30100pulsetest.py
+++ cow - physical module/30100pulsetest.py
@@ -1,9 +1,17 @@
 import max30100
+import time
+
 mx30 = max30100.MAX30100()
-mx30.refresh_temperature()
-temp = mx30.get_temperature()
-print('TEMP=%d'%temp)
-reg = mx30.get_registers()
-mx30.enable_spo2()
-mx30.read_sensor()
-mx30.ir, mx30.red
+while True:
+    mx30.refresh_temperature()
+    temp = mx30.get_temperature()
+
+    mx30.read_sensor()
+
+    print("Temperature: {:.2f} C".format(temp))  # Formatted temperature output
+    print("IR: {}".format(mx30.ir))
+    print("Red: {}".format(mx30.red))
+    print("-----------------")  # Optional separator between readings
+
+    time.sleep(1)  # Adjust delay as needed (1 second in this example) 
+
